367d4d7b3a9ec74a91927eded0dcdec4a34be182
theavinashk <avi09sh@gmail.com>
2025-01-04 20:37:47 +0530

    modified navbar
    Signed-off-by: theavinashk <avi09sh@gmail.com>

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -121,14 +121,36 @@ export default function NavBar() {
       {/* Right Content */}
       <NavbarContent justify="end">
         <button
-          className="px-4 py-2 bg-gray-200 dark:bg-gray-700 rounded"
+          className="px-4 py-2 rounded"
           onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
         >
-          ##
+          <svg
+            width="21"
+            height="21"
+            viewBox="0 2 22 22"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8V16Z"
+              fill="currentcolor"
+            ></path>
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM12 4V8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16V20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4Z"
+              fill="currentcolor"
+            ></path>
+          </svg>
         </button>
         <NavbarItem className="hidden lg:flex">
           <Link href="/lexmo">
             <Button color="default">Lexmo</Button>
+          </Link>
+        </NavbarItem>
+        <NavbarItem className="hidden lg:flex">
+          <Link href="/automize">
+            <Button color="default">Automize</Button>
           </Link>
         </NavbarItem>
       </NavbarContent>
