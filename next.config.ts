1aab85172749669e4b12721fb8e76e109fb5f7ca
theavinashk <avi09sh@gmail.com>
2025-01-17 17:42:37 +0530

    fixed build
    Signed-off-by: theavinashk <avi09sh@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  images: {
+    domains: ["localhost"],
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+      },
+    ],
+  },
   /* config options here */
 };
 
